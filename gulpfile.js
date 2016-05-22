731337faed007157cbe0ea9b6343330f212c8c80
Julian Rosenblum <jsrmath@gmail.com>
2016-05-21 22:11:10 -0400

    Update sharp11

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ var babel = require('babelify');
 
 gulp.task('build', function () {
   return browserify('./scripts/main.jsx', {debug: true})
-  	.transform(babel.configure({presets: ["react"]}))
-  	.bundle()
-	.pipe(source('bundle.js'))
+    .transform(babel.configure({presets: ["react"]}))
+    .bundle()
+    .pipe(source('bundle.js'))
     .pipe(gulp.dest('.'));
 });
 
