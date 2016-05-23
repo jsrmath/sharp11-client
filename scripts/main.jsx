b63cc60c38918021d3e3f6343a75d93726ba2dd3
Julian Rosenblum <jsrmath@gmail.com>
2016-05-22 22:58:55 -0400

    Refactored audio as separate npm module

--- scripts/main.jsx
+++ scripts/main.jsx
@@ -1,17 +1,17 @@
 var s11 = require('sharp11');
 var _ = require('underscore');
-var audio = require('./audio');
+var audio = require('sharp11-web-audio');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var songs = require('./songs');
 var Piano = require('./Piano.jsx');
 
-audio.init(function (play, stop) {
+audio.init(function (err, fns) {
   ReactDOM.render(
     <Piano
       range={[s11.note.create('C3'), s11.note.create('C6')]}
-      play={play}
-      stop={stop}
+      play={fns.play}
+      stop={fns.stop}
       defaultOctave={4}
       songs={songs} />,
     document.getElementById('content')
