8ea35e62f4aa5be89fe3016b2add80c7f3597fb3
Julian Rosenblum <jsrmath@gmail.com>
2017-11-28 11:29:57 -0500

    Fix formatting for theorizer

--- scripts/Theorizer.jsx
+++ scripts/Theorizer.jsx
@@ -8,18 +8,24 @@ module.exports = React.createClass({
 
   render: function () {
     return (
-      <div className="theorizer col-md-6">
-        <p>Enter a chord/scale to play or an interval to transpose selected keys by.</p>
-        <p>Examples: <em>Gm7b5</em> , <em>F Mixolydian</em> , <em>P4</em></p>
-        <div className="input-group">
-          <input type="text"
-                 className="form-control"
-                 value={this.props.value}
-                 onChange={this.props.handleInput}
-                 onKeyDown={this.handleKeyDown} />
-          <span className="input-group-btn">
-            <Button handleClick={this.props.play} text="Play" />
-          </span>
+      <div className="theorizer">
+        <div className="col-md-12">
+          <p>
+            Enter a chord/scale to play or an interval to transpose selected keys by, e.g.:
+            <em>Gm7b5</em> <em>F Mixolydian</em> <em>P4</em>
+          </p>
+        </div>
+        <div className="col-md-6">
+          <div className="input-group">
+            <input type="text"
+                   className="form-control"
+                   value={this.props.value}
+                   onChange={this.props.handleInput}
+                   onKeyDown={this.handleKeyDown} />
+            <span className="input-group-btn">
+              <Button handleClick={this.props.play} text="Play" />
+            </span>
+          </div>
         </div>
       </div>
     );
